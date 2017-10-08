848cf06f79eaad2c7bece3528157de69d66fd172
Frieda Uswatun <hallofrieda@gmail.com>
2017-10-09 00:08:41 +0700

    hampir jadi

--- client/client.py
+++ client/client.py
@@ -1,11 +1,12 @@
 import socket
 import os
+import email
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = 'localhost'
+host = raw_input('Masukkan IP host :')
 port = 3000
 client_socket.connect((host, port))
-location = "/home/frieda/PycharmProjects/5115100063_5115100071/client/downloads"
+location = "/home/frieda/PycharmProjects/5115100063_5115100071/progjar-tugas1/client/downloads"
 os.chdir(location)
 location = os.getcwd()
 
@@ -14,11 +15,16 @@ reqfile = raw_input('Input nama file yang ingin didownload :')
 client_socket.send(reqfile)
 unduh, file = reqfile.split()
 with open(file, 'wb') as file_to_write:
+    count = 0
     while(True):
         data = client_socket.recv(1024)
+        for char in data:
+            if count >= 2:
+                file_to_write.write(char)
+            if char == '\n':
+                count+=1
         if not data:
             break
-        file_to_write.write(data)
 file_to_write.close()
 print client_socket.recv(1024)
 client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,27 +1,57 @@
 import socket
 import os
-import sys
-import cgi
+import select
+import threading
+import Queue
 
-host = 'localhost'
+host = socket.gethostname()
 port = 3000
+print host
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host,port))
 serversocket.listen(10)
-os.chdir('/home/frieda/PycharmProjects/5115100063_5115100071/server/dataset')
+os.chdir('/home/frieda/PycharmProjects/5115100063_5115100071/progjar-tugas1/server/dataset')
 location = os.getcwd()
+
+open_client = []
+message_to_send = []
 print location
-while(True):
-    clientsocket, address = serversocket.accept()
-    data = clientsocket.recv(1024)
-    unduh, file = data.split()
-    print file
-    if os.path.isfile(file):
+
+q = Queue.Queue()
+
+class SenderThread(threading.Thread):
+    def __init__(self, file):
+        threading.Thread.__init__(self)
+        self.file = file
+    def run(self):
         print file
+        current_socket.send("client-id: klien-" + repr(q.get()))
+        current_socket.send("\n")
+        current_socket.send("file-size:" + repr(len(file)))
+        current_socket.send("\n")
         with open(file, 'rb') as file_to_send:
             for content in file_to_send:
-                clientsocket.send(content)
-    else:
-        clientsocket.send("File tidak ditemukan")
-    clientsocket.close()
-serversocket.close()
+                current_socket.send(content)
+
+while(True):
+    try:
+        rlist, wlist, xlist = select.select([serversocket] + open_client, open_client, [])
+        for current_socket in rlist:  # sockets that can be read
+            if current_socket is serversocket:  # if there is a new client
+                (new_socket, address) = serversocket.accept()
+                open_client.append(new_socket)  # clients list
+                id = len(open_client)
+                q.put(id)
+            else:
+                data = current_socket.recv(1024)
+                print data
+                unduh, file = data.split()
+                if len(data) == 0:
+                   open_client.remove(current_socket)  # remove user if he quit.
+                else:
+                    if os.path.isfile(file):
+                        t=SenderThread(file)
+                        t.run()
+                open_client.remove(current_socket)
+    except KeyboardInterrupt:
+        serversocket.close()
